18ea20243728d3d08865ddc6945dbcc379f09c9c
Wojciech <wbrozonowicz@o2.pl>
2018-11-06 20:34:01 +0100

    Java projects Semestr1

---
+++ src/semestr1/arrayTest.java
@@ -0,0 +1,13 @@
+package semestr1;
+
+public class arrayTest {
+
+    public static void main(String[] args) {
+
+        String[] names = {"Wojciech", "Marek", "Ela", "Jola", "Tomek"};
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
