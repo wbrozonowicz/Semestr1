f7a6f8db665fae0c7194321aadc7444134ae92d2
Wojciech <wbrozonowicz@o2.pl>
2018-11-07 18:54:59 +0100

    Java projects Semestr1

--- src/semestr1/BubbleSort.java
+++ src/semestr1/BubbleSort.java
@@ -2,31 +2,30 @@ package semestr1;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] a = {9, 3, 8, 4, 6, 77};
-        System.out.println("Tablica przed sortowaniem bąbelkowym:");
-        for (int i=0;i<a.length;i++){
-            System.out.print(a[i]+" ");
+        int[] inArray = {9, 3, 8, 4, 6, 77}; // array for bubble sort
+        sortMyArray(inArray);
+    }
+
+    static void sortMyArray(int[] array) {
+        System.out.println("Tablica przed sortowaniem:");
+        for (int a : array) {
+            System.out.print(a + "\t");
         }
         System.out.println("");
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < a.length - 1; j++) {
-                   int temp;
-                   if (a[j]>a[j+1]){
-                       temp = a[j+1];
-                       a[j+1]=a[j];
-                       a[j]=temp;
-                   }
-            }
-        }
-
         System.out.println("Tablica po sortowaniu:");
-        for (int i =0;i<a.length;i++){
-            System.out.print(a[i]+" ");
+        int temp = 0;
+        for (int i = 0; i < array.length; i++)
+            for (int j = 0; j < array.length - 1; j++) {
+
+                if (array[j] > array[j + 1]) {
+                    temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        for (int a : array) {
+            System.out.print(a + "\t");
         }
-
-
-
     }
 
 
